692dc5360efa66a0e09691da19bcea4de8bacfbe
melnicek <melnicek@outlook.com>
2021-05-22 15:46:51 +0200

    handling other youtube urls

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
-for (param of window.location.search.substr(1).split("&")){
-    if(param.split('=')[0] == 'v'){
-        window.location.href = "https://fyoutube.melnicek.xyz/?" + param.split('=')[1];
+if (window.location.href.indexOf("youtube.com/watch") > 0) {
+    for (param of window.location.search.substr(1).split("&")) {
+        if (param.split('=')[0] == 'v') {
+            window.location.href = "https://fyoutube.melnicek.xyz/?" + param.split('=')[1];
+        }
     }
+} else {
+    window.location.href = "https://is.muni.cz/auth";
 }
